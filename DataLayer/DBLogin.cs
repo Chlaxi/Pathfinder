c69d4d4c5d7a4f52dc23daab91813f38010e08bc
RÆVENS-PC\Chloe <raeven@live.dk>
2019-11-08 12:41:43 +0100

    DBLogin, secure file

---
+++ DataLayer/DBLogin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer
+{
+    public static class DBLogin
+    {
+        public static string DBInfo = "";
+    }
+}
