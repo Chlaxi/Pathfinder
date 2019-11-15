10ef61e9d2d58ecba840dd3e0e355bc252ff2b7c
Chlaxi <chlaxi.lunari@gmail.com>
2019-11-15 18:30:54 +0100

    Forgot to upload the DTO to add a spell to the spell list

---
+++ WebService/DTO/SpellToAddDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService
+{
+    public class SpellToAddDTO
+    {
+        public int SpellId { get; set; }
+        public int SpellLevel { get; set; }
+    }
+}
