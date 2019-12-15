b40c2fe9dbbed9250a0312d31e5fbb1a45a47d69
RÆVENS-PC\Chloe <raeven@live.dk>
2019-12-15 03:44:25 +0100

    Implementing bootstrap. Made a simple switching nav bar as well

--- WebService/wwwroot/js/app.js
+++ WebService/wwwroot/js/app.js
@@ -1,7 +1,59 @@
 ﻿define(["knockout", "spellModal"], function (ko, spellModal) {
 
 
+    var menuElements = ko.observableArray([
+        {
+            name: "Login",
+            component: "login-comp"
+        },
+        {
+            name: "Sign Up",
+            component: "signup-comp"
+        }
+    ]);
     var LoggedIn = ko.observable(false);
+
+     LoggedIn.subscribe(function (state) {
+         console.log("beep bop", state);
+         if (!state) {
+             menuElements([
+                 {
+                     name: "Login",
+                     component: "login-comp"
+                 },
+                 {
+                     name: "Sign Up",
+                     component: "signup-comp"
+                 }
+             ]);
+             ChangeContent(menu = { name: "Login", component: "login-comp"});
+         }
+         else {
+             menuElements([
+                 {
+                     name: "Characters",
+                     component: "player-front"
+                 }
+
+             ]);
+             ChangeContent(menu = {name: "character",component: "player-front"});
+         }
+     });
+
+    var currentMenu = ko.observable(menuElements()[0]);
+    var currentComponent = ko.observable(currentMenu().component);
+
+    var ChangeContent = function (menu) {
+        console.log("MENU", menu);
+        currentMenu(menu.name);
+        currentComponent(menu.component);
+    }
+
+    var isSelected = function (menu) {
+        return menu === currentMenu() ? "active" : "";
+    };
+
+
     var currentParams = ko.observable({});
     var Token = ko.observable();
     var CurrentPlayer = ko.observable({ id: undefined, name: "" });
@@ -14,8 +66,9 @@
 
     var sheetInfo = ko.observable({});
     var GoToSheet = function (data) {
+        //ChangeContent(menu = { name: "character sheet", component: "character" });
         sheetInfo(sheet = { active: true, id: data.id });
-        //ChangeContent(menu = {name: "character sheet",component: "character"});
+       
 
     };
 
@@ -32,6 +85,7 @@
     };
 
     return {
+        currentComponent, menuElements, ChangeContent, isSelected,
         LoggedIn,
         currentParams,
         Token,
